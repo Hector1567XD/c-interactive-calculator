f1f256c68a3b04ddd0bf99ac16cc306c153b1d8f
feredev <Hector1567XD@gmail.com>
2021-11-28 15:01:44 -0500

    Free the contextResponse in main

--- core/main.c
+++ core/main.c
@@ -55,17 +55,51 @@ void interactiveMode(Context* context) {
     if (strlen(context->response) > 0) printf(">%s\n", context->response);
   }
 
+  if (strlen(context->response) > 0) free(context->response);
+
   // Vuelve a ejecutar el interactiveModeExecution si es que el
   // programa sigue corriende
   if (programIsRunning == TRUE)
     interactiveMode(context);
 }
 
-int main()
+int main(int argQuantity, char* arg[])
 {
     Context* context = CreateProgramContext();
 
-    interactiveMode(context);
+    if (argQuantity < 2) {
+      interactiveMode(context);
+    }else{
+      int programIsRunning = TRUE;
+      char* errorDisplay = "";
+      for (int i = 0; i < argQuantity; i++) {
+
+        //context->input = arg[i];
+
+        executeProgram(context);
+
+        // Si detecta que ha habido un comando invalido
+        if (context->error == INVALID_COMMAND_ERROR) {
+          printf("Comando o accion invalida '%s' en el argumento %d", context->input, i);
+          break;
+        }
+        else if (strcmp(context->command, QUIT_COMMAND) == 0) {
+          programIsRunning = FALSE;
+        }else{
+          // Error displayer
+          errorDisplay = GetError(context->error);
+          if (strlen(errorDisplay) > 0) printf(">%s\n", errorDisplay);
+
+          // Response of Command (If is Neccesary)
+          if (strlen(context->response) > 0) printf(">%s\n", context->response);
+        }
+
+        // Vuelve a ejecutar el interactiveModeExecution si es que el
+        // programa sigue corriende
+        if (programIsRunning == TRUE)
+          interactiveMode(context);
+      }
+    }
 
     return 0;
 }
