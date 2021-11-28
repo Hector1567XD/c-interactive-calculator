6f2ca7ddcd7b228536465fb7ecfd936f9e245bfc
feredev <Hector1567XD@gmail.com>
2021-11-28 15:55:09 -0500

    Rama Comandos Linux

--- core/main.c
+++ core/main.c
@@ -70,34 +70,37 @@ int main(int argQuantity, char* arg[])
     if (argQuantity < 2) {
       interactiveMode(context);
     }else{
-      int programIsRunning = TRUE;
       char* errorDisplay = "";
-      for (int i = 0; i < argQuantity; i++) {
+      char* lastResponse = "";
+      int hasError = FALSE;
+      for (int i = 1; i < argQuantity; i++) {
 
         strcpy(context->input, arg[i]);
-
         executeProgram(context);
 
         // Si detecta que ha habido un comando invalido
         if (context->error == INVALID_COMMAND_ERROR) {
-          printf("Comando o accion invalida '%s' en el argumento %d", context->input, i);
+          printf("Comando o accion invalida '%s' en el argumento %i\n", context->input, i);
+          hasError = TRUE;
           break;
-        }
-        else if (strcmp(context->command, QUIT_COMMAND) == 0) {
-          programIsRunning = FALSE;
+        }else if (strcmp(context->command, QUIT_COMMAND) == 0) {
+          hasError = TRUE;
+          break;
         }else{
-          // Error displayer
+
           errorDisplay = GetError(context->error);
-          if (strlen(errorDisplay) > 0) printf(">%s\n", errorDisplay);
+          if (strlen(errorDisplay) > 0) {
+            hasError = TRUE;
+            printf("Error en argumento '%s' numero %i: %s\n", context->input, i, errorDisplay);
+            break;
+          }
 
-          // Response of Command (If is Neccesary)
-          if (strlen(context->response) > 0) printf(">%s\n", context->response);
+          if (strlen(context->response) > 0) lastResponse = context->response;
         }
-
-        // Vuelve a ejecutar el interactiveModeExecution si es que el
-        // programa sigue corriende
-        if (programIsRunning == TRUE)
-          interactiveMode(context);
+      }
+      if (strlen(lastResponse) > 0 && !hasError) {
+        printf("%s\n", lastResponse);
+        free(lastResponse);
       }
     }
 
