a0a5f49d5fd65987d4efe814a8a27fc49ea8d481
Anna <annabellacadena@gmail.com>
2021-11-28 17:57:02 -0400

    Rama resta fuera del core

---
+++ commands/execute-resta.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../utils/include/booleans.h"
+#include "../utils/include/pila.h"
+
+#include "../core/include/errors.h"
+#include "../core/include/context.h"
+
+void executeResta(Context* context)
+{
+    context->error = NO_ERRORS;
+    char resultado[45] = "";
+
+    int pilaSize = getPilaLongitud(context->numberStack);
+    if (pilaSize < 2) {
+      context->error = INSUFICIENT_VALUES_ERROR;
+      return;
+    }
+
+    double numeroA = TomarCimaYDesapilar(context->numberStack);
+    double numeroB = TomarCimaYDesapilar(context->numberStack);
+
+    double operacion = numeroB - numeroA;
+
+    Apilar(context->numberStack, operacion);
+
+    snprintf( resultado, 46, "%g", operacion );
+    context->response = resultado;
+}
