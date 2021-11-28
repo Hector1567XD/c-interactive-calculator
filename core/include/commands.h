fb7ff26fe1a0fcd583d368c002a8e4588321808e
yisustxz <jesusoortiz2410@gmail.com>
2021-11-27 22:51:24 -0500

    defines

--- core/include/commands.h
+++ core/include/commands.h
@@ -2,18 +2,18 @@
 #define commands_h
 
 // ACTION COMMANDS
-extern const char* QUIT_COMMAND = "QUIT";
+#define QUIT_COMMAND "QUIT"
 
 // ARITMETICS COMMANDS
-extern const char* SUMA_COMMAND = "+";
-extern const char* MULTIPLICAR_COMMAND = "*";
+#define SUMA_COMMAND "+"
+#define MULTIPLICAR_COMMAND "*"
 
 // ESPECIAL COMMANDS
-extern const char* NO_COMMAND = "NO_COMMAND";
-extern const char* COMMAND_ERROR = "COMMAND_ERROR";
-extern const char* ADD_NUMERIC_VALUE = "ADD_NUMERIC_VALUE";
+#define NO_COMMAND "NO_COMMAND"
+#define COMMAND_ERROR "COMMAND_ERROR"
+#define ADD_NUMERIC_VALUE "ADD_NUMERIC_VALUE"
 
 // EASTER EGG
-extern const char* IMPOSIBLE_COMMAND = "IMPOSIBLE_COMMAND";
+#define IMPOSIBLE_COMMAND "IMPOSIBLE_COMMAND"
 
 #endif
--- core/include/errors.h
+++ core/include/errors.h
@@ -1,10 +1,10 @@
 #ifndef errors_h
 #define errors_h
 
-extern const int NO_ERRORS = 0;            // :)
-extern const int INPUT_MAX_LENGTH_ERROR = 1;
-extern const int INVALID_COMMAND_ERROR = 2;
+#define NO_ERRORS 0            // :)
+#define INPUT_MAX_LENGTH_ERROR 1
+#define INVALID_COMMAND_ERROR 2
 
-extern const int INSUFICIENT_VALUES_ERROR = 3;
+#define INSUFICIENT_VALUES_ERROR 3
 
 #endif
