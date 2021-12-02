4092588adf05fee89612c6368ba22f1bc7e61be2
Anna <annabellacadena@gmail.com>
2021-12-01 21:40:36 -0400

    corregido error con getError

--- core/get-error.c
+++ core/get-error.c
@@ -8,11 +8,8 @@ char* GetError(int errorCode)
 {
 
      if (errorCode == UNDEFINED) return "Valor indefinido";
-         return ""; 
-
      if (errorCode == INSUFICIENT_VALUES_ERROR) return "No hay suficientes valores en la pila.";
-         return ""; 
-
      if (errorCode == TOO_MANY_VALUES) return "Ingrese solo un numero";
-         return "";      
+    
+     return "";      
 }
