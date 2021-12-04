d6025aae572bd121b83e86bc795c2a39222412fd
Feredev <hector1567xd@gmail.com>
2021-12-03 22:56:36 -0400

    Encapsular el shellMode dentro de una funcion (shellMode en este caso)

--- core/main.c
+++ core/main.c
@@ -63,6 +63,40 @@ void interactiveMode(Context* context) {
     interactiveMode(context);
 }
 
+void shellMode(Context* context) {
+  char* errorDisplay = "";
+  char* lastResponse = "";
+  int hasError = FALSE;
+  for (int i = 1; i < argQuantity; i++) {
+
+    strcpy(context->input, arg[i]);
+    executeProgram(context);
+
+    if (context->error == INVALID_COMMAND_ERROR) {
+      printf("Comando o accion invalida '%s' en el argumento %i\n", context->input, i);
+      hasError = TRUE;
+      break;
+    }else if (strcmp(context->command, QUIT_COMMAND) == 0) {
+      hasError = TRUE;
+      break;
+    }else{
+
+      errorDisplay = GetError(context->error);
+      if (strlen(errorDisplay) > 0) {
+        hasError = TRUE;
+        printf("Error en argumento '%s' numero %i: %s\n", context->input, i, errorDisplay);
+        break;
+      }
+
+      if (strlen(context->response) > 0) lastResponse = context->response;
+    }
+  }
+  if (strlen(lastResponse) > 0 && !hasError) {
+    printf("%s\n", lastResponse);
+    free(lastResponse);
+  }
+}
+
 int main(int argQuantity, char* arg[])
 {
     Context* context = CreateProgramContext();
@@ -70,37 +104,7 @@ int main(int argQuantity, char* arg[])
     if (argQuantity < 2) {
       interactiveMode(context);
     }else{
-      char* errorDisplay = "";
-      char* lastResponse = "";
-      int hasError = FALSE;
-      for (int i = 1; i < argQuantity; i++) {
-
-        strcpy(context->input, arg[i]);
-        executeProgram(context);
-
-        if (context->error == INVALID_COMMAND_ERROR) {
-          printf("Comando o accion invalida '%s' en el argumento %i\n", context->input, i);
-          hasError = TRUE;
-          break;
-        }else if (strcmp(context->command, QUIT_COMMAND) == 0) {
-          hasError = TRUE;
-          break;
-        }else{
-
-          errorDisplay = GetError(context->error);
-          if (strlen(errorDisplay) > 0) {
-            hasError = TRUE;
-            printf("Error en argumento '%s' numero %i: %s\n", context->input, i, errorDisplay);
-            break;
-          }
-
-          if (strlen(context->response) > 0) lastResponse = context->response;
-        }
-      }
-      if (strlen(lastResponse) > 0 && !hasError) {
-        printf("%s\n", lastResponse);
-        free(lastResponse);
-      }
+      shellMode(context);
     }
 
     return 0;
